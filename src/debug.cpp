e6a54d675c4ea1e5920b82579352c6317baba65d
Carlos Carracedo garcía-villalba <ccg-v@hotmail.com>
2025-03-19 14:02:08 +0100

    QUIT working, broadcasting leave message to channel/s pending

--- src/debug.cpp
+++ src/debug.cpp
@@ -6,7 +6,7 @@
 /*   By: ccarrace <ccarrace@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 03:02:37 by ccarrace          #+#    #+#             */
-/*   Updated: 2025/03/18 01:12:22 by ccarrace         ###   ########.fr       */
+/*   Updated: 2025/03/19 13:38:50 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@ void	Server::_debugListClients()
 {
 	std::map<int, Client>::iterator it;
 
-	std::cout << "[~DEBUG]:\tList of clients connected to server: " << std::endl;
-	for (it = this->_clients.begin(); it != this->_clients.end(); it++)
+	if (this->_clients.empty())
+		std::cout << "[DEBUG~]:\tNo clients connected to server" << std::endl;
+	else
 	{
-		std::cout << "\t\t- " << it->first << ": " << it->second.getNickname() << std::endl;
+		std::cout << "[~DEBUG]:\tList of clients connected to server: " << std::endl;
+
+		for (it = this->_clients.begin(); it != this->_clients.end(); it++)
+		{
+			std::cout << "\t\t- " << it->first << ": " << it->second.getNickname() << std::endl;
+		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
 }
 
 void Server::_debugListChannels()
@@ -31,55 +37,57 @@ void Server::_debugListChannels()
     if (this->_channels.empty())
         std::cout << "[DEBUG~]:\tNo channels on server" << std::endl;
     else
+	// {
         std::cout << "[~DEBUG]:\tList of channels and their members: " << std::endl;
 
-	// Iterate over all channels stored in server (std::map<std::string, Channel> _channels in Server)
-    for (channelIt = this->_channels.begin(); channelIt != this->_channels.end(); channelIt++)
-    {
-        std::cout << "[~DEBUG]:\t- " << channelIt->first << std::endl;
+		// Iterate over all channels stored in server (std::map<std::string, Channel> _channels in Server)
+		for (channelIt = this->_channels.begin(); channelIt != this->_channels.end(); channelIt++)
+		{
+			std::cout << "[~DEBUG]:\t- " << channelIt->first << std::endl;
 
-        std::vector<int> members = channelIt->second.getClients();
+			std::vector<int> members = channelIt->second.getClients();
 
-		// Iterate over the members of a channel (std::vector<int> _clients in Channel)
-        for (size_t i = 0; i < members.size(); i++)
-        {
-            int fd = members[i];
-            std::string channelName = channelIt->first;
+			// Iterate over the members of a channel (std::vector<int> _clients in Channel)
+			for (size_t i = 0; i < members.size(); i++)
+			{
+				int fd = members[i];
+				std::string channelName = channelIt->first;
 
-            // Check if client exists
-            Client *member = _findClientByFd(fd);
-            if (!member) {
-                std::cerr << "Error: Client with fd " << fd << " not found among " << channelName << " members!" << std::endl;
-                continue;
-            }
+				// Check if client exists
+				Client *member = _findClientByFd(fd);
+				if (!member) {
+					std::cerr << "Error: Client with fd " << fd << " not found among " << channelName << " members!" << std::endl;
+					continue;
+				}
 
-            // Check if fd exists in _clients map
-            if (_clients.find(fd) == _clients.end()) {
-                std::cerr << "Error: FD " << fd << " not found in clients map!" << std::endl;
-                continue;
-            }
+				// Check if fd exists in _clients map
+				if (_clients.find(fd) == _clients.end()) {
+					std::cerr << "Error: FD " << fd << " not found in clients map!" << std::endl;
+					continue;
+				}
 
-            //	// Check if member has any subscriptions to
-            std::map<std::string, bool> subscriptions = member->getChannels();
-            if (subscriptions.empty()) {
-                // std::cerr << "Warning: Client " << member->getNickname() << " has no channel subscriptions!" << std::endl;
-                continue;
-            }
+				//	// Check if member has any subscriptions to
+				std::map<std::string, bool> subscriptions = member->getChannels();
+				if (subscriptions.empty()) {
+					// std::cerr << "Warning: Client " << member->getNickname() << " has no channel subscriptions!" << std::endl;
+					continue;
+				}
 
-            std::map<std::string, bool>::iterator it = subscriptions.find(channelName);
+				std::map<std::string, bool>::iterator it = subscriptions.find(channelName);
 
-            std::cout << "\t\t · ";
-            if (it != subscriptions.end())
-            {
-                if (it->second == true)  
-                    std::cout << "+o ";
-                else
-                    std::cout << "-o ";
-            }
-			std::cout << _clients[fd].getNickname() << std::endl;      
-        }
-    }
-    std::cout << std::endl;
+				std::cout << "\t\t · ";
+				if (it != subscriptions.end())
+				{
+					if (it->second == true)  
+						std::cout << "+o ";
+					else
+						std::cout << "-o ";
+				}
+				std::cout << _clients[fd].getNickname() << std::endl;      
+			}
+		}
+		std::cout << std::endl;
+	// }	
 }
 
 // void	Server::_debugListChannels()
--- src/quit.cpp
+++ src/quit.cpp
@@ -6,7 +6,7 @@
 /*   By: ccarrace <ccarrace@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 18:20:08 by ccarrace          #+#    #+#             */
-/*   Updated: 2025/03/19 01:12:03 by ccarrace         ###   ########.fr       */
+/*   Updated: 2025/03/19 14:00:09 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,23 @@ void	Server::_quit(Client &client, const t_tokens msgTokens)
 	// Iterate over the client's subscription
 	for (it = clientChannels.begin(); it != clientChannels.end(); ++it)
 	{
-		std::map<std::string, bool>::iterator next = it; // Store the next iterator
-		++next; // Move to the next element before erasing
+		// std::map<std::string, bool>::iterator next = it; // Store the next iterator
+		// ++next; // Move to the next element before erasing
 
 		std::string channelName = it->first; // <-- AQUI ACONSEGUEIXO EL NOM DEL CANAL AL QUAL ESTA SUBSCRIT
 
 		// Find the channel object by name
-		// Channel *channel = this->_findChannelByName(channelName); // <-- INSTANCIO EL CANAL...
+		Channel *channel = this->_findChannelByName(channelName); // <-- DESO EL PUNTER AL CANAL
 		// std::vector<int>& channelClients = channel->getClientsByRef();	// ... I ACCEDEIXO A LA LLISTA DE MEMBRES
 
 		// // Remove the client among the channel's members
 		// std::vector<int>::iterator clientIt = std::find(channelClients.begin(), channelClients.end(), client.getFd());
 		// channelClients.erase(clientIt);
 
-		// channel->removeMember(client.getFd());
-		// client.unsubscribe(channelName);
-		std::cout << "Client to remove from " << channelName << ": " << client.getNickname() << "(" << client.getFd() << ")" << std::endl;
-		std::cout << "Subscription to remove from " << client.getNickname() << ": " << channelName << std::endl;
+		/**********************************************************************/
+		/*         BROADCAST QUIT TO MEMBERS OF THE CHANNELS TO LEAVE         */
+		/**********************************************************************/		
+		channel->removeMember(client.getFd());
+		client.unsubscribe(channelName);
 	}
-
-	std::cout << "=============== QUIT ===============" << std::endl;
-	_debugListChannels();
-	std::cout << "====================================" << std::endl;
 }
